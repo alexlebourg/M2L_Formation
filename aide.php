10cd4fddcd3deec589d401014f098cfe4586e085
alexlebourg <alexlebourg@yahoo.fr>
2015-04-27 22:26:38 +0200

    Rubrique d'aide - Fin

--- aide.php
+++ aide.php
@@ -45,7 +45,12 @@ session_start();
 		    		<li><a id="lienEmploye" href="#leg">Mentions Légales</a></li>
 		    		<li><a id="lienEmploye" href="#formation">Accès aux formations</a></li>
 		    		<li><a id="lienEmploye" href="#infoPerso">Accès aux informations personnelles</a></li>
-		    		<li><a id="lienEmploye" href="#respon">Vous êtes responsable</a></li>
+		    		<li><a id="lienEmploye" href="#respon">Vous êtes responsable : Consultez les formations de vos employés</a></li>
+		    		<li><a id="lienEmploye" href="#rh">Vous êtes au service RH : Gérez les formations</a>
+		    			<ul><li><a id="lienEmploye" href="#ajout">Ajouter une formation</a></li>
+		    				<li><a id="lienEmploye" href="#affecte">Affecter une formation à un employé</a></li>
+		    			</ul>
+		    		</li>
 		    	</ol>
 		    	
 		    	<br><br>
@@ -89,13 +94,36 @@ session_start();
 			    <br><br>
 			    <p>
 			    	<h2 id="respon">Consultation des formations de vos employés</h2>
-			    	Etant responsable de votre service, vous avez accès à la lecture des forations de vos employés.
+			    	Etant responsable de votre service, vous avez accès à la lecture des formations de vos employés.
 			    	<br>Pour cela, aller dans la rubrique de vos formations, sous les deux premiers tableaux récapitulant vos données de formations, un affichage supplémentaire présente les employés sous votre responsabilité :
 			    	<p><img id="image" src="aideImage/ListeEmployeResponsable.png" alt="Photo non chargée" title="ListeEmployeResponsable.png"></p>
 			    	Cliquer sur un employé et vous optiendrez le détail de celui-ci : ces informations, le récaptulatif des CPF et la liste de ses foramtions
 			    	<p><img id="image" src="aideImage/DetailEmploye.png" height="90%" width="90%" alt="Photo non chargée" title="DetailEmploye.png"></p>
 		    	</p>
-		    	
+		    	<br><br>
+			    <p>
+			    	<h2 id="rh">Service Ressources Humaines</h2>
+			    	Etant au service des Ressources Humaines, vous devez gérer les demandes de formations. Vous avez donc accès aux formations de tous les employés.
+			    	Pour les consulter, aller dans la rubrique de vos formations, sous les deux premiers tableaux récapitulant vos données de formations, un affichage supplémentaire présente la liste de tous les employés en 2 catégories (salariés et responsabes) :
+			    	<p><img id="image" src="aideImage/ListeEmployesRH.png" alt="Photo non chargée" title="ListeEmployesRH.png"></p>
+			    	<p>
+			    		<h2 id="ajout">Ajouter une formation</h2>
+			    		Au bas de la page, au-dessous des listes listes d'employé, vous trouverez la rubrique ajouter une formation.
+			    		<p><img id="image" src="aideImage/BoutonAjoutForma.png" alt="Photo non chargée" title="ListeEmployesRH.png"></p>
+			    		Cliquer sur "Nouvelle Formation". Vous serez redirigé vers le formulaire d'ajout : 
+			    		<p><img id="image" src="aideImage/FormulaireAjoutFormation.png" alt="Photo non chargée" title="FormulaireAjoutFormation.png"></p>
+			    		Validez, la formation est ajoutée.
+			    	</p>
+			    	<p>
+			    		<h2 id="affecte">Affecter une formation à un employé</h2>
+			    		Pour affecter une formation à un employé, celle-ci doit déjà exister. Dans le cas contraire, entrez la formation en suivant les instructions de la rubrique "<a id="lienEmploye" href="#ajout"<i><u>Ajouter une formation</u></i></a>".
+			    		<br>Ensuite, allez sur la page de l'employé souhaité. Pour celà, cliquez sur un nom dans la liste. Au bas de la page, sous les informations de l'employé, cliquez sur "Affecter une formation". Le formulaire suivant s'affiche :
+			    		<p><img id="image" src="aideImage/AffecteFormation.png" alt="Photo non chargée" title="AffecteFormation.png"></p>
+			    		Selectionnez la formation dans la liste, entrez la date, puis validez.<br>La formation a été affectée à l'employé.
+			    	</p>
+			    	
+			    
+			    </p>
 		    </div>
 		    <?php include 'presentation/footer.php';?>
     	</div>
